547d1d03b1e7186c216ba5e0d69384a4a301ff4f
Trai Nguyen <tnguyen@at-pac.com>
2019-10-17 09:33:15 -0400

    added hexagon

---
+++ assets/js/style.js
@@ -0,0 +1,27 @@
+// Styling the expanded menu function
+var j=0;
+var i=document.getElementById("menu").childNodes;
+function expand() {
+    if (j==0) {
+        document.getElementById("add").style.transform = "rotate(45deg)";
+        document.getElementById("menu").style.transform = "scale(1)";
+        i[1].style.transform="translateY(-160px)";
+        i[3].style.transform="translate(140px, -80px)";
+        i[5].style.transform="translate(140px, 80px)";
+        i[7].style.transform="translateY(160px)";
+        i[9].style.transform="translate(-140px, 80px)";
+        i[11].style.transform="translate(-140px, -80px)";
+        j=1;
+
+    } else {
+        document.getElementById("add").style.transform = "rotate(0deg)";
+        document.getElementById("menu").style.transform = "scale(1)";
+        i[1].style.transform="translateY(0)";
+        i[3].style.transform="translate(0)";
+        i[5].style.transform="translate(0)";
+        i[7].style.transform="translateY(0)";
+        i[9].style.transform="translate(0)";
+        i[11].style.transform="translate(0)";
+        j=0;
+    }
+}
