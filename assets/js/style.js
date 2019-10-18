bce8b91aec3c82d396cd946b9637ad27f768908b
Trai Nguyen <tnguyen@at-pac.com>
2019-10-18 16:30:20 -0400

    added popup menu for acaddemic

--- assets/js/style.js
+++ assets/js/style.js
@@ -5,12 +5,12 @@ function expand() {
     if (j==0) {
         document.getElementById("add").style.transform = "rotate(45deg)";
         document.getElementById("menu").style.transform = "scale(1)";
-        i[1].style.transform="translateY(-160px)";
-        i[3].style.transform="translate(140px, -80px)";
-        i[5].style.transform="translate(140px, 80px)";
-        i[7].style.transform="translateY(160px)";
-        i[9].style.transform="translate(-140px, 80px)";
-        i[11].style.transform="translate(-140px, -80px)";
+        i[1].style.transform="translateY(-190px)"; // top
+        i[3].style.transform="translate(160px, -100px)"; //top right
+        i[5].style.transform="translate(160px, 100px)"; //bottom right
+        i[7].style.transform="translateY(190px)"; //bottom
+        i[9].style.transform="translate(-160px, 100px)"; //bottom left
+        i[11].style.transform="translate(-160px, -100px)"; //top left
         j=1;
 
     } else {
